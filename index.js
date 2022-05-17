eecb479627d3cdfb1809bf1e999f10fe0b34775b
Hieu <mh1755603@gmail.com>
2022-05-17 08:42:37 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const morgan = require("morgan");
+app.use(morgan("combined"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
